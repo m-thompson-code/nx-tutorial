28d81d9bf251f77c6ae88bad563d0c6a3ad5adbc
Mark Thompson <mark.thompson@smpl.company>
2021-01-05 16:51:17 -0800

    Display Todos

--- apps/todos/src/app/app.component.ts
+++ apps/todos/src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+interface Todo {
+  title: string;
+}
+
 @Component({
   selector: 'myorg-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'todos';
+  todos: Todo[] = [
+    {
+      title: 'Todo 1',
+    },
+    {
+      title: 'Todo 2',
+    },
+  ];
+
+  public addTodo(): void {
+    this.todos.push({
+      title: `New todo ${Math.floor(Math.random() * 1000)}`,
+    });
+  }
 }
